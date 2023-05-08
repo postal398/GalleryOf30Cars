d671e8eae7ee1f3b638ff48918509738c7c3ac76
postal398 <postal398@ya.ru>
2023-05-08 10:02:49 +0300

    add main work class, and full it

--- app/src/main/java/com/example/galleryof30cars/MainActivity.kt
+++ app/src/main/java/com/example/galleryof30cars/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.galleryof30cars
 import android.util.Log
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.galleryof30cars.ui.theme.GalleryOf30CarsTheme
-import data.Iamtest
-import data.fields
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                     GalleryApp()
                 }
             }
@@ -35,8 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GalleryApp(){
-    Log.d("TAG", "message")
-    Log.d(Iamtest, "hey")
-    Log.d(fields.toString(), "fields")
-}
+    Column() {
+        Text(text = "Hello world")
 
+    }
+
+//        Log.d(ImgList().toString(), "ImgList")
+
+
+}
